4f6c61eb639ccc35411b9ade3ebf2db30e992bbf
Hermann <hermannc125@gmail.com>
2024-10-14 08:39:18 -0400

    Added a warning if it is snowy (only for snowy for some reason)

--- CastleBravo.py
+++ CastleBravo.py
@@ -10,8 +10,14 @@ from time import sleep
 
 
 def weather():
-    weatherForcast = ["sunny", "raining", "cloudy", "snowing", "icy", "windy"]
+    weatherForcast = ["sunny", "rain7", "cloudy", "snowy", "icy", "windy"]
     return weatherForcast[random.randint(0,len(weatherForcast)-1)] #overly complicated way of doing random.choice but I made it so it's staying
 
+weatherAlert = weather()
 
-print (weather())
+def VRS():
+    if weatherAlert == "snowy":
+        return "The national Weather Serivce has update out alarm by 30 minutes because of the forcast of " + weatherAlert + " weather conditions."
+    else:
+        return "Fine"
+print(VRS())
